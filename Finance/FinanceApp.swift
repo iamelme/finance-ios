5265b83afc4b3380acdf529311e2a3d2cd5b186d
iamelme <elme.delossantos@gmail.com>
2024-01-28 15:06:52 +0800

    Initial Commit

---
+++ Finance/FinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceApp.swift
+//  Finance
+//
+//  Created by Elme delos Santos on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct FinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
